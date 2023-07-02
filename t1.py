6387ab90ca4f1a8895ee94b2c5c47f8b5e9bfda4
Ananya <shuklagungun5@gmail.com>
2023-07-02 12:36:00 +0530

    conditions and turtle

---
+++ t1.py
@@ -0,0 +1,18 @@
+from turtle import *
+speed('slowest')
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+
+hideturtle() # hide the turtle
+
+mainloop() # hold the window open
