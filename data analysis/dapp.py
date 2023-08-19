e29afec73c905c92efdc63f92144a451234ab76b
ananyacode <shuklagungun5@gmail.com>
2023-08-19 14:35:39 +0530

    data_analysis_1

---
+++ data analysis/dapp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+#run in terminal
+#cd data analysis
+#streamlit run dapp.py
+import numpy as np
+import pandas as pd
+import plotly.express as px 
+import seaborn as sns
+
+st.title('Data Analysis App')
+
+
